f7610bdadc363a15dce3ff7ea940946ffb688231
bahram <bsamani@samani.com>
2016-12-05 15:04:36 -0500

    add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
+var moment = require('moment');
 
 var TodoApp = require('TodoApp');
 
@@ -19,13 +20,18 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    // expect cratedAt to be a number
+    expect (todoApp.state.todos[0].createdAt).toBeA('number');
+
   });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: 'Test features',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos: [todoData]});
@@ -33,5 +39,28 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    //expect compelte at to be a number
+
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
   });
+
+  it('should toggle todo from completed to incompleted', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    //expect compelte at to be a number
+
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
+  });
+  // test toggle from true to false completed at gets removed
 });
