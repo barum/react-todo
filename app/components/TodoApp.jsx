09044d894dbc7c61daf85bbc2fadad6eb345aecd
bahram <bsamani@samani.com>
2016-12-07 16:10:29 -0500

    refactor todolist todo and addtodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,14 +2,16 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment= require('moment');
 
+import TodoList from 'TodoList';
+//var TodoList = require('TodoList');
+import AddTodo from 'AddTodo';
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+
 var TodoSearch = require('TodoSearch');
 var TodoAPI=require('TodoAPI');
 
 
-var TodoApp = React.createClass ({
+export var TodoApp = React.createClass ({
   getInitialState: function () {
     return {
       showCompleted: false,
@@ -34,20 +36,7 @@ var TodoApp = React.createClass ({
       ]
     });
   },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed? moment().unix():undefined;
-      }
-
-      return todo;
-    });
-    this.setState({
-      todos: updatedTodos
-    });
-  },
-  handleSearch: function (showCompleted, searchText) {
+    handleSearch: function (showCompleted, searchText) {
     this.setState({
       showCompleted: showCompleted,
       searchText: searchText.toLowerCase()
@@ -64,7 +53,7 @@ var TodoApp = React.createClass ({
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
               <TodoSearch onSearch={this.handleSearch}/>
-              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <TodoList/>
               <AddTodo onAddTodo = {this.handleAddTodo}/>
             </div>
           </div>
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,7 +1,7 @@
-var moment = require('moment');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
-export var searchTextReducer = (state='', action) => {
+export var searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
       return action.searchText;
@@ -10,18 +10,16 @@ export var searchTextReducer = (state='', action) => {
   };
 };
 
-
-export var showCompletedReducer = (state=false, action) => {
+export var showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
     default:
       return state;
-  };
+  }
 };
 
-
-export var  todosReducer = (state=[], action) => {
+export var todosReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -38,15 +36,17 @@ export var  todosReducer = (state=[], action) => {
       return state.map((todo) => {
         if (todo.id === action.id) {
           var nextCompleted = !todo.completed;
+
           return {
             ...todo,
             completed: nextCompleted,
-            completedAt: nextCompleted ? moment.unix() : undefined
+            completedAt: nextCompleted ? moment().unix() : undefined
           };
+        } else {
+          return todo;
         }
       });
     default:
       return state;
   }
-
 };
